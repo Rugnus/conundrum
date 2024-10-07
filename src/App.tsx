f28a852e7d8e830189fb244f9d2aed471053c61a
SYGasanov <sygasanov@get-lab.ru>
2024-10-07 18:54:39 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { AboutPage } from "@pages/AboutPage/AboutPage";
+import { BlindTypePage } from "@pages/BlindTypePage/BlindTypePage";
 import { CatalogPage } from "@pages/CatalogPage/CatalogPage";
 import { MainPage } from "@pages/MainPage/MainPage";
 import { Route, Routes } from "react-router-dom";
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<MainPage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/blind-type" element={<BlindTypePage />} />
     </Routes>
   );
 }
