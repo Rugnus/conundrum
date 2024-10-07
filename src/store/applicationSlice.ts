e84eb66560ac79b2b8d9912651a0cc11b46d0484
SYGasanov <sygasanov@get-lab.ru>
2024-10-07 15:24:22 +0300

    add app slice and initial state for it

---
+++ src/store/applicationSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Application {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+}
+
+interface ApplicationState {
+  list: Application[];
+}
+
+const initialState: ApplicationState = {
+  list: [
+    {
+      id: 1,
+      name: "Touch Typing Trainer",
+      description:
+        "Train your touch typing skills with this interactive typing app.",
+      image:
+        "https://wl-ideasen5minutos.cf.tsp.li/resize/1200x630/jpg/3d2/3a7/e4333251b09ef10f5bfce9910a.jpg", // замените на реальный URL
+    },
+  ],
+};
+
+export const applicationSlice = createSlice({
+  name: "applications",
+  initialState,
+  reducers: {
+    setApplications(state, action: PayloadAction<Application[]>) {
+      state.list = action.payload;
+    },
+    addApplication(state, action: PayloadAction<Application>) {
+      state.list.push(action.payload);
+    },
+  },
+});
+
+export const { setApplications, addApplication } = applicationSlice.actions;
+export default applicationSlice.reducer;
