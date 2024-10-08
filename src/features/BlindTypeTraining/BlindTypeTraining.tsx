71a181eb9bcf5b0b2ed330861c5c993e778d3149
SYGasanov <sygasanov@get-lab.ru>
2024-10-08 19:41:03 +0300

    add blind type training v1.0

--- src/features/BlindTypeTraining/BlindTypeTraining.tsx
+++ src/features/BlindTypeTraining/BlindTypeTraining.tsx
@@ -1,5 +1,79 @@
-import { FC } from "react";
+import { Button, pxToRem } from "@conundrum/ui-kit";
+import { Input } from "antd";
+import { FC, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+
+const SBlindTypeTraining = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: ${pxToRem(15)};
+  align-items: center;
+  margin: ${pxToRem(30)} auto;
+`;
+
+const SInputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  input {
+    text-align: center;
+    font-size: ${pxToRem(24)};
+    width: 100%;
+    height: ${pxToRem(70)};
+    letter-spacing: ${pxToRem(2)};
+  }
+`;
 
 export const BlindTypeTraining: FC = () => {
-  return <div>BlindTypeTraining</div>;
+  const [userInput, setUserInput] = useState<string>("тренировка печати");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, []);
+
+  const fetchRandomSentence = async () => {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    console.log(data);
+
+    return data.content;
+  };
+
+  const handleRandomSentence = async () => {
+    const newSentence = await fetchRandomSentence();
+    console.log(newSentence);
+
+    setUserInput(newSentence);
+  };
+
+  const handleKeyChange = (
+    e: React.KeyboardEvent<HTMLInputElement> &
+      React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const keyPressed = e.key;
+
+    if (keyPressed === userInput[0]) {
+      setUserInput((prev) => prev.slice(1));
+      if (userInput.length === 1) {
+        handleRandomSentence();
+      }
+    } else {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <SBlindTypeTraining>
+      <SInputWrapper>
+        <Input value={userInput} onKeyDown={handleKeyChange} readOnly />
+      </SInputWrapper>
+
+      <Button title="Начать тренировку" />
+    </SBlindTypeTraining>
+  );
 };
